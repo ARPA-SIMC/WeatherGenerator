d94abf2f0028d2c11146839f7615422bf108371c
voltAntonio <avolta@arpa.emr.it>
2022-03-03 10:03:42 +0100

    cambio dati input

--- WG2D/test_WG2D_Eraclito/main.cpp
+++ WG2D/test_WG2D_Eraclito/main.cpp
@@ -131,7 +131,7 @@ int main(int argc, char *argv[])
     int nrYearSimulations = NR_SIMULATION_YEARS;
     //printf("insert the number of years of the the synthethic series:\n");
     //scanf("%d",&nrYearSimulations);
-    nrYearSimulations = 100;
+    nrYearSimulations = 60;
     time_t rawtime;
     struct tm * timeinfo;
     time ( &rawtime );
@@ -168,7 +168,7 @@ int main(int argc, char *argv[])
     //fp = fopen("./inputData/list_C4.txt","r"); // !! take out
     //fp = fopen("./inputData/list_C7_Russi.txt","r"); // !! take out
     //fp = fopen("./inputData/list_farini_00296.txt","r"); // !! take out
-    fp = fopen("./inputData/centro.txt","r"); // !! take out
+    fp = fopen("./inputData/est.txt","r"); // !! take out
     //fp = fopen("./inputData/fausto.txt","r");
 
     int numberOfCells; // !! take out
@@ -182,7 +182,7 @@ int main(int argc, char *argv[])
     //fp = fopen("./inputData/list_C4.txt","r"); // !! take out
     //fp = fopen("./inputData/list_C7_Russi.txt","r"); // !! take out
     //fp = fopen("./inputData/list_farini_00296.txt","r"); // !! take out
-    fp = fopen("./inputData/centro.txt","r"); // !! take out
+    fp = fopen("./inputData/est.txt","r"); // !! take out
     //fp = fopen("./inputData/fausto.txt","r");
 
     int* cellCode = nullptr; // !! take out
